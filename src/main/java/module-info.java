efa1f790e30d1b5e27806a5269c9cf5d6ca4278e
Ariana Sánchez <ardasanc@espol.edu.ec>
2022-01-21 20:14:44 -0500

    Adiccion del pom y correo

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,7 +1,10 @@
-module com.mycompany.proyecto.poo {
+module com.mycompany.proyectopoojar {
     requires javafx.controls;
     requires javafx.fxml;
+    requires java.base;
+    requires java.mail;
 
-    opens com.mycompany.proyecto.poo to javafx.fxml;
-    exports com.mycompany.proyecto.poo;
+    opens com.mycompany.proyectopoojar to javafx.fxml;
+    opens com.mycompany.modelo to javafx.base;
+    exports com.mycompany.proyectopoojar;
 }
